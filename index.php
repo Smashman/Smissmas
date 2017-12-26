65cd2be100928ceb84fe8108ddaa08036a721dd5
Ben Williams <ben@smash.mn>
2017-12-26 21:09:50 +0000

    Finished

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>Nightmare Before Smissmas</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="css/main.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="css/credits.css">
+        <link rel="shortcut icon" href="favicon.png">
+    </head>
+     <body>
+        <div class="container" id="container_01">
+            <div class="content" id="content_01">
+                <a class="button" id="tf2" href="http://teamfortress.com/" target="_blank"></a>
+            </div>
+        </div>
+        <div class="container" id="container_02">
+            <div class="content" id="content_02">
+            </div>
+        </div>
+        <div class="container" id="container_03">
+            <div class="content" id="content_03">
+                <a class="maps halloween" id="cauldron" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1159639999" target="_blank">
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <img class="image" src="images/maps/cauldron_1.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/cauldron_2.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/cauldron_3.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/cauldron_4.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/cauldron_5.jpg"/>
+                    </div>
+                    <div class="frame"></div>
+                </a>
+                <a class="maps xmas" id="stormy" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1233357991" target="_blank">
+                    <div class="frame"></div>
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <img class="image" src="images/maps/stormy_1.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/stormy_2.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/stormy_3.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/stormy_4.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/stormy_5.jpg"/>
+                    </div>
+                </a>
+                <a class="maps halloween" id="spookey" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1156801718" target="_blank">
+                    <div class="frame"></div>
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <img class="image" src="images/maps/spookey_1.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/spookey_2.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/spookey_3.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/spookey_4.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/spookey_5.jpg"/>
+                    </div>
+                </a>
+                <a class="maps xmas" id="turbine" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1231741449" target="_blank">
+                    <div class="frame"></div>
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <img class="image" src="images/maps/turbine_1.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/turbine_2.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/turbine_3.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/turbine_4.jpg"/>
+                        <img class="image no-js-hide" src="images/maps/turbine_5.jpg"/>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="container" id="container_04">
+            <div class="content" id="content_04">
+                <div class="taunt" id="accordion">
+                    <div class="blurb">Earn your title as Polka King of the Badlands.</div>
+                    <a class="button taunt_vid" href="https://www.youtube.com/watch?v=G7se0k_VDvE" data-vid="G7se0k_VDvE" target="_blank"></a>
+                    <a class="created" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1209210655" target="_blank">Created by Populus, NeoDement, and SedimentarySocks</a>
+                </div>
+            </div>
+        </div>
+        <div class="container" id="container_05">
+            <div class="content" id="content_05">
+                <div class="taunt" id="skate">
+                    <div class="blurb">Go for the gold! It’s topical now!</div>
+                    <a class="button taunt_vid" href="https://www.youtube.com/watch?v=zxRE457kngQ" data-vid="zxRE457kngQ" target="_blank"></a>
+                    <a class="created" target="_blank" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1216056098">Created by Hypo, Donhonk, and Petachepas</a>
+                </div>
+            </div>
+        </div>
+        <div class="container" id="container_06">
+            <div class="content" id="content_06">
+                <div class="taunt" id="pogo">
+                    <div class="blurb">The higher you go, the less liable we are.</div>
+                    <a class="button taunt_vid" href="https://www.youtube.com/watch?v=JH9QxC-BxHE" data-vid="JH9QxC-BxHE" target="_blank"></a>
+                    <a class="created" target="_blank" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1203636693">Created by Nonamesleft~, NeoDement, and Square</a>
+                </div>
+                <div class="taunt" id="carpet">
+                    <div class="blurb">It’s a whole new world. Hang on tight!</div>
+                    <a class="button taunt_vid" href="https://www.youtube.com/watch?v=mXzfNhGEBlw" data-vid="mXzfNhGEBlw" target="_blank"></a>
+                    <a class="created" target="_blank" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1171598880">Created by Populus, Square, and Greg</a>
+                </div>
+            </div>
+        </div>
+        <div class="container" id="container_07">
+            <div class="content" id="content_07">
+                <div class="paints" id="hwn_paints">
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <a class="paint" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1181531026" target="_blank">
+                            <img class="image" src="images/paints/hwn_5.png"/>
+                            <div class="name">Totally Boned</div>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=525593593" target="_blank">
+                            <img class="image" src="images/paints/hwn_1.png"/>
+                            <div class="name">Arachnophobia</div>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1188108295" target="_blank">
+                            <img class="image" src="images/paints/hwn_2.png"/>
+                            <div class="name">Beep Boy Brushed</div>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1177990156" target="_blank">
+                            <img class="image" src="images/paints/hwn_3.png"/>
+                            <div class="name">Black Lodged</div>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1174305528" target="_blank">
+                            <img class="image" src="images/paints/hwn_4.png"/>
+                            <div class="name dl">Spirit of Halloween</div>
+                        </a>
+                    </div>
+                </div>
+                <div class="paints" id="xms_paints">
+                    <div class="slides">
+                        <div class="arrow prev no-js-hide"></div>
+                        <div class="arrow next no-js-hide"></div>
+                        <a class="paint" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1181631450" target="_blank">
+                            <div class="name">Polar Surprise</div>
+                            <img class="image" src="images/paints/xms_3.png"/>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1193923444" target="_blank">
+                            <div class="name">Knit Kitted</div>
+                            <img class="image" src="images/paints/xms_1.png"/>
+                        </a>
+                        <a class="paint munchable no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1201340479" target="_blank">
+                            <div class="name dl">Munchable Mercenaries</div>
+                            <img class="image" src="images/paints/xms_2.png"/>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1221902888" target="_blank">
+                            <div class="name">Secret Snowfall</div>
+                            <img class="image" src="images/paints/xms_4.png"/>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1181081883" target="_blank">
+                            <div class="name">Festive Wrapped</div>
+                            <img class="image" src="images/paints/xms_5.png"/>
+                        </a>
+                        <a class="paint no-js-hide" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1197407887" target="_blank">
+                            <div class="name">Frost Ornamented</div>
+                            <img class="image" src="images/paints/xms_6.png"/>
+                        </a>
+                    </div>
+                </div>
+                <a class="button" id="assets" href="https://tf2maps.net/downloads/diva-dans-ice-pack.5553/" target="_blank"></a>
+                <a class="button" id="workshop" href="http://steamcommunity.com/sharedfiles/filedetails/?id=1235294227" target="_blank"></a>
+                <div id="thanks">
+                    Special Thanks to <a href="http://mods.tf/" target="_blank" class="emph">mods.tf</a>, <a href="http://tf2maps.net/" target="_blank" class="emph">TF2Maps.net</a>, the <a href="https://discord.gg/mtm73eV" class="emph">TF2Maps Discord</a>, and the <a href="https://discord.gg/AwD4Vvs" class="emph">TF2 Emporium Discord</a>. Thanks also to the TF2 community for your amazing efforts - your work perpetuates this community and the people within it, driving us all to succeed and accomplish our dreams. Last but not least, thanks to everyone at Valve for making this all possible - we couldn’t have done any of this without you!
+                </div>
+            </div>
+        </div>
+        <div class="container" id="container_08">
+            <div class="content" id="content_08">
+                <?php include 'php/credits.php';?>
+                <div id="credits">
+                    <div class="row">
+                        <div>
+                            <?php steam_block(87084440,"Thumbnail Design, SFM Renders & Web Assets",true);?>
+                            <a href="//twitter.com/mellowretro" class="button twitter" target="_blank"></a>
+                        </div>
+                        <div>
+                            <?php steam_block(33885066,"Management");?>
+                            <a href="//twitter.com/aeonbollig" class="button twitter" target="_blank"></a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div>
+                            <?php steam_block(39933033,"Web Developer");?>
+                            <a href="//twitter.com/smashman2004" class="button twitter" target="_blank"></a>
+                        </div>
+                    </div>
+                </div>
+                <div id="text_credits">
+                    <span class="emph">Spectral Snowflake</span> created by Donhonk, Psyke & Colteh.
+                    <br/>
+                    <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=1236765708" target="_blank" class="emph">Saxton Hale</a> model created by Maxxy & JPRAS.
+                    <br/>
+                    <a href="http://steamcommunity.com/sharedfiles/filedetails/?id=1185537363" target="_blank" class="emph">Enhanced Merasmus</a> model created by Alaxe & The Forbidden Fruit.
+                </div>
+                <div id="legal"></div>
+            </div>
+        </div>
+        
+        <div id="shade">
+            <div id="zoomholder">
+            </div>
+        </div>
+        <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script src="scripts/jquery.cycle2.min.js" type="text/javascript"></script>
+        <script src="scripts/main.js" type="text/javascript"></script>
+    </body>
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,76 @@
+$(function(){
+    $("body").addClass("js-enabled");
+
+    shade = $("#shade");
+
+    shade.click(function(){shade.fadeOut(200);vidframe.remove();});
+
+    $(".taunt .taunt_vid").click(function(ev){
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        var vidid = ev.currentTarget.dataset.vid;
+        vidframe = $("<iframe class=\"video\" src=\"http://www.youtube.com/embed/" + vidid + "?showinfo=0&amp;vq=hd720&amp;rel=0&amp;autohide=1&amp;autoplay=1\" frameborder=\"0\" allowfullscreen=\"\"></iframe>");
+        shade.children().append(vidframe);
+
+        shade.fadeIn(200);
+    });
+
+    $('#cauldron .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .next',
+        prev:   '> .prev',
+        timeout: 4000,
+        delay:   100
+    });
+
+    $('#stormy .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .next',
+        prev:   '> .prev',
+        timeout: 4000,
+        delay:   200
+    });
+
+    $('#spookey .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .next',
+        prev:   '> .prev',
+        timeout: 4000,
+        delay:   300
+    });
+
+    $('#turbine .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .next',
+        prev:   '> .prev',
+        timeout: 4000,
+        delay:   400
+    });
+
+    $('#hwn_paints .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .next',
+        prev:   '> .prev',
+        timeout: 4000,
+        slides:  "> a",
+        random: true
+    });
+
+    $('#xms_paints .slides').cycle({
+        fx:     'scrollHorz',
+        speed:   200,
+        next:   '> .prev',
+        prev:   '> .next',
+        timeout: 4000,
+        slides:  "> a",
+        random: true,
+        reverse: true
+    });
+
+});
